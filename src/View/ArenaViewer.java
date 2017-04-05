aa1329b0d1154fd43bd723454906efcea7845a2d
joseph <joseph@josephh-Aspire-M5-583P>
2017-04-05 18:58:25 -0500

    Content, again

--- src/View/ArenaViewer.java
+++ src/View/ArenaViewer.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 import Arena.Arena;
 import java.util.ArrayList;
 import Arena.Droid;
+import java.awt.ComponentOrientation;
 import java.awt.GridLayout;
 
 /**
@@ -19,16 +20,16 @@ import java.awt.GridLayout;
  */
 public class ArenaViewer extends JPanel implements ActionListener {
     
-    private Arena arena;
-    private ArrayList<ArrayList<PixelComponent>> grid;
+    final private Arena arena;
+    final private ArrayList<ArrayList<PixelComponent>> grid;
     private ArrayList<Integer[]> lines;
     
     public void actionPerformed(ActionEvent e) {
         ArrayList<Integer[]> moves = arena.getDroidMoves();
         ArrayList<Integer[]> lasers = arena.getLasers();
         for (Integer[] m : moves) {
-            grid.get(m[0].intValue()).get(m[1].intValue()).clear();
-            grid.get(m[2].intValue()).get(m[3].intValue()).set(m[4]);
+            grid.get(m[0]).get(m[1]).clear();
+            grid.get(m[2]).get(m[3]).set(m[4]);
         }
         lines = lasers;
     }
@@ -49,7 +50,15 @@ public class ArenaViewer extends JPanel implements ActionListener {
         for (Droid d : arena.getParticipants()) {
             grid.get(d.getX()).get(d.getY()).update(d.getAlignment());
         }
-        JPanel.setLayout(new GridLayout(arena.getWidth(), arena.getHeight()));
+        this.setLayout(new GridLayout(arena.getWidth(), arena.getHeight()));
+        //Grid Layout unfortunately requires me to add a row before adding the next one, making logic
+        //a bit clunky. Regardless:
+        for (int i = 0; i < arena.getHeight(); ++i) {
+            for (ArrayList<PixelComponent> column : grid) {
+                this.add(column.get(i));
+            }
+        }
+        
     }
     
     
