770cea9407d43c08486d0fd47d51fe44c79f10bd
joseph <joseph@josephh-Aspire-M5-583P>
2017-04-05 18:21:07 -0500

    More content

---
+++ src/View/PixelComponent.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import java.awt.Color;
+import java.awt.Component;
+
+/**
+ *
+ * @author joseph
+ */
+public class PixelComponent extends Component {
+    private Color color;
+    
+    public void clear() {
+        
+    }
+    public void update(int droidID) {
+        
+    }
+    public PixelComponent(Color color) {
+        this.color = color;
+    }
+    
+    public PixelComponent() {
+        this.color = 
+    }
+    
+}
