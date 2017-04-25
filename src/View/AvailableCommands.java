5597c74be801854fe796cccfb4cdf20d72f7ebcd
joseph <joseph@josephh-Aspire-M5-583P>
2017-04-25 09:32:04 -0500

    This branch is bad, so I'm committing my progress and getting rid of it.

---
+++ src/View/AvailableCommands.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import Arena.Level;
+import javax.swing.JScrollPane;
+
+/**
+ *
+ * @author joseph
+ */
+public class AvailableCommands extends JScrollPane {
+    Commands 
+    public AvailableCommands(Level l) {
+        
+    }
+}
