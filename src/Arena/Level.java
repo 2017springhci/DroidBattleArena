d14d5d4805b26fa4f0952552e66a53a4cb60a73f
joseph <joseph@josephh-Aspire-M5-583P>
2017-04-25 09:31:47 -0500

    This branch is bad, so I'm committing my progress and getting rid of it.

---
+++ src/Arena/Level.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arena;
+
+import Program.Program;
+import java.awt.Dimension;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author joseph
+ */
+public class Level implements Serializable{
+    //A level has an arena, a set of available commands, a set of enemy droids,
+    //and a droid for the player.
+    private Arena arena;
+    private ArrayList<Command>
+    
+}
