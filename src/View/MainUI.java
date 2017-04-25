2b6e7f7b4764f89fdefef1627bd835232cd1b4bc
joseph <joseph@josephh-Aspire-M5-583P>
2017-04-25 17:03:09 -0500

    Added in the actual arena viewer, modified dimension.

--- src/View/MainUI.java
+++ src/View/MainUI.java
@@ -7,6 +7,7 @@ package View;
 
 import Arena.Arena;
 import Arena.Droid;
+import Program.DragnDropFrame;
 import Program.Program;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -26,17 +27,16 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  * @author joseph
  */
 public class MainUI extends JFrame {
-    private ArenaViewer arenaviewer;
-    private Arena arena;
-    private JScrollPane commands;
-    private JScrollPane code;
-    private JMenuBar menu;
-    private JMenu file;
-    private JMenuItem loadPlayerProgram;
-    private JMenuItem loadEnemyProgram;
-    private JMenuItem createProgram;
-    private JMenuItem loadLevel;
-    private JPanel content;
+    private final ArenaViewer arenaViewer;
+    private final Arena arena;
+    private final JMenuBar menu;
+    private final JMenu file;
+    private final JMenu play;
+    private final JMenuItem battle;
+    private final JMenuItem loadPlayerProgram;
+    private final JMenuItem loadEnemyProgram;
+    private final JMenuItem createProgram;
+    private final JPanel content;
     public static void main(String[] args ) {
         MainUI toRun = new MainUI();
         toRun.setVisible(true);
@@ -46,7 +46,7 @@ public class MainUI extends JFrame {
     
     public MainUI () {
         super();
-        arena = new Arena(60, 30);
+        arena = new Arena(30, 60);
         arena.addParticipant(new Droid (9, 9, new Program()));
         arena.addParticipant (new Droid (49, 19, new Program()));
         BorderLayout layout = new BorderLayout();
@@ -62,35 +62,38 @@ public class MainUI extends JFrame {
 		fc.setFileFilter(new FileNameExtensionFilter(".dba"));
 		int returnVal = fc.showOpenDialog(this);
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			Arena.getPlayer(0).setProgram(Program.loadProgram(fc.getSelectedFile()));
+			arena.getParticipants().get(0).setProgram(Program.loadProgram(fc.getSelectedFile()));
 		}
                 
         });
+        file.add(loadEnemyProgram);
         loadEnemyProgram.addActionListener(e -> 
         {       JFileChooser fc = new JFileChooser();
 		fc.setFileFilter(new FileNameExtensionFilter(".dba"));
 		int returnVal = fc.showOpenDialog(this);
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			Arena.getPlayer(1).setProgram(Program.loadProgram(fc.getSelectedFile()));
+			arena.getParticipants().get(1).setProgram(Program.loadProgram(fc.getSelectedFile()));
 		}
                 
         });
 		//view.setFocus();
-        file.add(saveProgram);
         file.add(createProgram);
+        createProgram.addActionListener(e ->
+        {
+            new DragnDropFrame();
+        });
         menu.add(file);
-        level = new JMenu("Level");
-        loadLevel = new JMenuItem("Select level");
-        
-        level.add(loadLevel);
-        menu.add(level);
+        play = new JMenu("Play");
+        battle = new JMenuItem("Battle");
+        battle.addActionListener(e -> {
+            arena.runGame();
+        });
+        play.add(battle);
+        menu.add(play);
         content.add(menu,BorderLayout.PAGE_START);
-        
-        
-        commands = new JScrollPane(); 
-        code = new JScrollPane();
-        content.setPreferredSize(new Dimension(1800, 675));
-  
+        arenaViewer = new ArenaViewer(arena);
+        content.add(arenaViewer);
+        content.setPreferredSize(new Dimension(625, 340));
         this.add(content);
         this.pack();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
